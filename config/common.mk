3aaa45f70c4086a5968f8707493cf65448c65db0
Danny Lin <danny@kdrag0n.dev>
2021-08-05 01:51:10 -0700

    common: Enable Gboard side padding
    This adds padding to the left and right sides of the keyboard when using
    Gboard.
    
    In portrait mode, there is 4dp of padding to align the keyboard with
    some UI elements and separate the keys from the edge of the screen.
    
    In landscape mode, there is 64dp of padding to reduce the overall key
    width, which makes the keyboard significantly easier to use.
    
    TODO: It might be better to set these in individual device trees, but
    for now, these props seem to work well for all official devices.
    
    Suggested-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -138,6 +138,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/afterlife/prebuilt/common/bin/wipe-frp.sh:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/wipe-frp
 
+# Gboard side padding
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.kb_pad_port_l=4 \
+    ro.com.google.ime.kb_pad_port_r=4 \
+    ro.com.google.ime.kb_pad_land_l=64 \
+    ro.com.google.ime.kb_pad_land_r=64
+
 # Lineage Health
 PRODUCT_COPY_FILES += \
     vendor/afterlife/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
