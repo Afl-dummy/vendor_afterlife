90ab7088b489d1a3f43d4cb6c302a7fb98b70c49
rmp22 <195054967+rmp22@users.noreply.github.com>
2024-06-17 19:55:36 +0900

    config: add blur support
    Change-Id: I5fabdbbdfe812346f17dbba5d4bf9abe7344e0ca
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -17,6 +17,20 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
+endif
+
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
