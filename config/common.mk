cb7349d709b6761c98bd43ae699ce6f3f1cf6d69
Danny Lin <danny@kdrag0n.dev>
2025-07-01 12:44:48 +0000

    config: Disable touch video heatmap
    Change-Id: Iac858eeb059a854dbb020d14a0f165b41055511d
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -147,6 +147,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=true
 
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
 # DeviceAsWebcam
 ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
     PRODUCT_PACKAGES += \
